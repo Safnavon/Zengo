35b65d5e557c3182e8215bbcbe22df19f0b45738
Asaf Navon <asafnavon@engage.do>
2021-11-27 15:36:23 +0200

    error handling

--- routes/coins.js
+++ routes/coins.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios')
-//https://min-api.cryptocompare.com/data/pricehistorical?fsym=USD&tsyms=BTC,ETH&ts=1452680400
-//https://min-api.cryptocompare.com/data/v2/histoday?fsym=ETH&tsym=BTC&aggregate=1&toTs=1452680400&limit=1
 const API_URL = "https://min-api.cryptocompare.com/"
 const API_KEY = "983f8a86f211d17fa8ba47e44969bc79aede178b5b66ba6a3bd56a224fea24a4"
 const API_COMPARE_PATH = "data/pricehistorical"
 
 
-/* GET users listing. */
+//{coins:[],date:MM/DD/YYYY}
 router.post('/compare', async function (req, res, next) {
+    if(!req.body){
+        res.status(400).send("request has no body, please send {coins:[],date:MM/DD/YYYY}")
+
+    }
     let {coins, date} = req.body;
-    if (!coins || !date) {
-        res.status(400).send("missing coins/date params")
+    if (!coins || !coins.length || !date) {
+        res.status(400).send("missing coins/date params, please send {coins:[],date:MM/DD/YYYY}")
     }
 
     let historyDateMillis = Math.floor(new Date(date).getTime() / 1000)
